768538ec651848ba1c678045e539e32c5611096e
wrefinity <wrefordmessi@gmail.com>
2026-03-22 14:41:21 +0100

    code modularization

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,8 +5,12 @@ import { authService } from './authService';
 import { userService } from './userService';
 import { contentService } from './contentService';
 import { forumService } from './forumService';
+import { executiveService } from './executiveService';
+import { foundingLeaderService } from './foundingLeaderService';
 
 export const authAPI = authService;
 export const userAPI = userService;
 export const contentAPI = contentService;
 export const forumAPI = forumService;
+export const executiveAPI = executiveService;
+export const foundingLeaderAPI = foundingLeaderService;
